7638739e9e46c6d84375d00473d35fb792937dc0
mrsins02 <amin.d.d.y1357@gmail.com>
2024-08-16 03:16:47 +0330

    DRF installed

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Party
+    "rest_framework",
+
+    # Local
     'chatapp.users',
 ]
 
@@ -120,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
