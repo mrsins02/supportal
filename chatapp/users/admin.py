f4bb97c60f6386f3226289236d3194bbc3462f2e
mrsins02 <amin.d.d.y1357@gmail.com>
2024-08-17 21:40:37 +0330

    User admin added

--- chatapp/users/admin.py
+++ chatapp/users/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        'id',
+        'phone_number',
+        'username',
+        'first_name',
+        'last_name',
+        "is_active"
+
+    )
+    list_display_links = (
+        "id",
+        "phone_number",
+    )
+    list_filter = (
+        'is_active',
+        'is_superuser',
+        'groups'
+    )
+    search_fields = (
+        'username',
+        'email',
+        'first_name',
+        'last_name'
+    )
+    readonly_fields = (
+        "id",
+    )
+    fieldsets = (
+        (None, {"fields": (
+            "username",
+            "password",
+            "otp_code"
+        )}),
+        ("Personal info", {"fields": (
+            "avatar",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "email"
+        )}),
+        ("Permissions", {"fields": (
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions"
+        )}),
+        ("Important dates", {"fields": (
+            "last_login",
+            "date_joined"
+        )}),
+    )
